0a0fd3728a85d04241bae229a905172ca0c56670
James Fox <foxjc3@gmail.com>
2020-12-10 20:01:19 -0500

    Files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
+
+console.log(ufoData);
+
+var tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+
+data.forEach(function(ufoData) {
+    console.log
+
+
+}
