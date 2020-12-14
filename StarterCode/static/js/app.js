c8d6ce6f9a8ff133f56ac57ebd766c8859dc219d
James Fox <foxjc3@gmail.com>
2020-12-14 14:07:32 -0500

    Files for hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,70 @@
 // from data.js
+
+//Worked with Jamie and Victor on this 
+
+
 var tableData = data;
 
+let tableBody = d3.select("tbody");
 
-console.log(ufoData);
+function createUFOTable(tableData)
 
-var tbody = d3.select("tbody");
+{
 
-// YOUR CODE HERE!
+//Clear out table
+tableBody.html("");
+ 
+//Loop through the list of dictionaries
 
-data.forEach(function(ufoData) {
-    console.log
+    tableData.forEach(ufoSighting => {
+        var row = tableBody.append("tr");
+        row.append("td").text(ufoSighting.datetime);
+        row.append("td").text(ufoSighting.city)
+        row.append("td").text(ufoSighting.state)
+        row.append("td").text(ufoSighting.country)
+        row.append("td").text(ufoSighting.shape)
+        row.append("td").text(ufoSighting.durationMinutes)
+        row.append("td").text(ufoSighting.comments)
+    });
+};
 
+// UFO Table
 
+createUFOTable(tableData);
+
+// Define filter  
+
+let filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", ufoFiltering);
+
+// Filter data table 
+function dateFilter(dt) {
+    let inputSearchDate = d3.select("#datetime");
+    let inputSearchDateValue = inputSearchDate.property("value");
+    if (inputSearchDateValue)
+        return dt.datetime === inputSearchDateValue;  
+    return true;
 }
+
+function ufoFiltering() {
+    console.log("Event Fired");
+    d3.event.preventDefault();
+    
+    
+// Remove Table 
+    d3.select("tbody").selectAll("tr").remove();
+    let ufoFilteredData = tableData.filter(dateFilter);
+
+    ufoFilteredData.forEach(ufoSighting => {
+        var row = tableBody.append("tr");
+        row.append("td").text(ufoSighting.datetime);
+        row.append("td").text(ufoSighting.city)
+        row.append("td").text(ufoSighting.state)
+        row.append("td").text(ufoSighting.country)
+        row.append("td").text(ufoSighting.shape)
+        row.append("td").text(ufoSighting.durationMinutes)
+        row.append("td").text(ufoSighting.comments)
+
+    });
+};
